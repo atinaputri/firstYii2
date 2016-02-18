a47c03b9dfe6c6a52d458a06eda126691ecc6993
atinaputri <putri.atina@gmail.com>
2016-02-18 07:30:10 +0700

    adding folder

---
+++ views/site/about.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+    	Finally bisa bikin website pake yii2!! YAAY!!
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
---
+++ views/site/say.php
@@ -0,0 +1,8 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<h1> Hello <?= Html::encode($target)?></h1>
+<p> Welcome to your Yii2 demonstration app. </p>
